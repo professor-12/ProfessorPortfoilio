48bec093dd62a1aede545af782f55d793a0bdc6a
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-01-19 19:50:28 +0100

    Edit example.env filename to .env.example

--- src/lib/Data.ts
+++ src/lib/Data.ts
@@ -1,6 +1,5 @@
 const API_KEY = process.env.API_KEY
 export async function getData() {
-
       const data = await  fetch("https://api.netlify.com/api/v1/sites", {
         headers: {
             "Content-Type": "application/json",
